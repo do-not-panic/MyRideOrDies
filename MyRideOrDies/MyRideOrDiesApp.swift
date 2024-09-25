33d6fc16f10177c6aeae353fe6a457939c914ad4
do-not-panic <schach@gmail.com>
2024-09-25 13:58:26 +0200

    Initial Commit

---
+++ MyRideOrDies/MyRideOrDiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRideOrDiesApp.swift
+//  MyRideOrDies
+//
+//  Created by René Pfammatter on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyRideOrDiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
